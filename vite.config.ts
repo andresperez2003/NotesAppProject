00c556c0207af35c6acb0fb3d6e0d330e5ad60ef
andresperez2003 <andresap2017@gmail.com>
2025-09-03 21:07:41 -0500

    bug: deploy failed

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/NotesAppProject/', // 👈 este es el nombre de tu repo
+  base: '/', // Cambiar a raíz para Netlify
 })
