6e0e768dd2dc27e9292205828ccd4c82251f45dc
andresperez2003 <andresap2017@gmail.com>
2025-09-07 21:49:19 -0500

    modifying backend route

--- src/services/paths.ts
+++ src/services/paths.ts
@@ -1,3 +1,3 @@
-// export const PATH_BACKEND="https://e0efc49b-4d7c-4219-aaae-9b935982d521.clouding.host"
+export const PATH_BACKEND="https://e0efc49b-4d7c-4219-aaae-9b935982d521.clouding.host"
 
-export const PATH_BACKEND="http://localhost:8080"
+//export const PATH_BACKEND="http://localhost:8080"
